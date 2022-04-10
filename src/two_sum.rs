e8647bae587225c692090e49b175dd9409bfb319
matianqi <mtqmx3@gmail.com>
2022-04-10 11:35:52 +0800

    update bench

--- src/two_sum.rs
+++ src/two_sum.rs
@@ -21,16 +21,11 @@ mod tests {
         assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
         assert_eq!(Solution::two_sum(vec![3, 2, 4], 6), vec![1, 2]);
     }
-
-    #[test]
-    fn test_2() {
-        assert_eq!(Solution::two_sum(vec![3, 3], 6), vec![0, 1]);
-    }
 }
 
 
 #[cfg(test)]
-mod bench {
+mod benchs {
     extern crate test;
     use self::test::Bencher;
     use super::Solution;
